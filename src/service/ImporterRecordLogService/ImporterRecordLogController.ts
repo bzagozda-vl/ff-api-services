d3357e50c3208ca5a5b762809b77ce05af5173ef
Bartosz Zagozda <bzagozda@valuelogic.one>
2022-01-17 11:18:07 +0100

    Change importer record log to use lambda

--- src/service/ImporterRecordLogService/ImporterRecordLogController.ts
+++ src/service/ImporterRecordLogService/ImporterRecordLogController.ts
@@ -2,14 +2,23 @@ import { APIClient, APIMapping } from '../../http';
 
 export class ImporterRecordLogController extends APIClient {
     constructor() {
-        super(APIMapping.lambdaProxyService);
+        super(APIMapping.flowfactImporterLambda);
     }
 
     async fetchLogDirectories() {
-        return this.invokeApiWithErrorHandling('/ff-importer-v2-record-log-s3-directory-entries', 'GET');
+        return this.invokeApiWithErrorHandling('/record-log/directory/content', 'GET');
     }
 
     async fetchDirectoryContents(table: string) {
-        return this.invokeApiWithErrorHandling('/ff-importer-v2-record-log-s3-directory-content', 'POST', { table });
+        return this.invokeApiWithErrorHandling(
+            `/record-log/directory/entries`,
+            'GET',
+            {},
+            {
+                queryParams: {
+                    table,
+                },
+            }
+        );
     }
 }
